a38bfb1e139d5f1865ca0511e326f37481a0b6ec
Chris <mayc2@rpi.edu>
2015-10-29 12:13:47 -0400

    adding hw5

---
+++ hw5/hw5.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+import scipy
+import math
+import matplotlib.pyplot as plt
+import random
+from Bio import SeqIO
+
+
+def main():
+
+	return 0
+
+if __name__ == "__main__":
+	sys.exit(main())
