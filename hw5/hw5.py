d0aa60f9e0eb1fdd54784ea709678fe91ac1ed64
Chris <mayc2@rpi.edu>
2015-10-29 12:37:06 -0400

    adding files to hw5 directory

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -7,7 +7,17 @@ import random
 from Bio import SeqIO
 
 
+def parse(filename):
+	answer = list(SeqIO.parse(open(filename),"fasta"))
+	return answer
+
 def main():
+
+	#parse in the PRP file
+	filename = "PRP.fasta"
+	records = parse(filename)
+
+	print(records)
 
 	return 0
 
