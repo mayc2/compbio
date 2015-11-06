0da0c6cb1ea07ec179c2e05084ba4148554dce6c
Chris <mayc2@rpi.edu>
2015-11-06 16:57:30 -0500

    adding stuff from hw5

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -35,24 +35,163 @@ def initConsensus(sequences):
 		answer[key] = temp
 	return answer
 
-def calculateMutualInfo(count_matrix):
-	M = initM(count_matrix)
+def convert(count_matrix):
+	L = len(count_matrix['A'])
 
+	frequency_matrix = count_matrix
+	for key in ['A','C','U','G','-']:
+		for i in range(len(frequency_matrix[key])):
+			frequency_matrix[key][i] /= L
+	return frequency_matrix
+
+def calculateMutualInfo(frequency_matrix,sequences):
+
+	#initialize matrix
+	L = len(frequency_matrix['A'])
+	M = np.zeros([L,L])
+
+	def combined_freq(i,j,n1,n2):
+		answer = 0.0
+		for seq in sequences:
+			if seq[i] == n1 and seq[j] == n2:
+				answer += 1
+		return answer
+
+	def find1(i,j):
+		answer = 0.0
+		for n1 in ['A','C','G','U','-']:
+			for n2 in ['A','C','G','U','-']:
+				if delta(n1,n2) == 1:
+					answer += combined_freq(i,j,n1,n2)
+		return answer
+
+	def find2(i,j):
+		answer = 0.0
+		for n1 in ['A','C','G','U','-']:
+			for n2 in ['A','C','G','U','-']:
+				f_i = frequency_matrix[n1][i]
+				f_j = frequency_matrix[n2][j]
+				f_ij = 0.0
+				if delta(n1,n2) == 1:
+					f_ij = combined_freq(i,j,n1,n2)
+				if (f_i != 0 and f_j != 0 and f_ij != 0):	
+					answer += math.log(f_ij / (f_i *f_j))
+		return answer 
+
+	for i in range(L):
+		for j in range(L):
+			if i != j:
+				value1 = find1(i,j)
+				value2 = find2(i,j)
+				answer = value1 * value2	
+				if answer < 0:
+					M[i][j] = 0.0
+				else:
+					M[i][j] = answer
 	return M
 
-def initM(count_matrix):
-	answer = []
-	for _ in range(len(count_matrix['A'])):
-		temp = []
-		for _ in range(len(count_matrix['A'])):
-			temp.append(0.0)
-		answer.append(temp)
-	return answer
+def delta(si, sj):
+    """
+    delta - score base pairs for Nussinov algorithm
+    si, si - nucleotide letters
+    return
+    d - delta score 1 for complement or G-U wobble pair, otherwise 0
+        1 indicates possible base pairng
+    """
+    
+    nt2int = {'A': 0, 'C': 1, 'G': 2, '-': 4, 'U': 3}
+    nt2comp = {'A': 3, 'C': 2, 'G': 1, '-': 5, 'U': 0}
+
+    d = 0
+    if nt2int[si] == nt2comp[sj]:
+        d = 1
+    elif (si == 'G' and sj == 'U') or (si == 'U' and sj == 'G'):
+        d = 1
+
+    return d
+
+def dynamic_program(M):
+	
+	#initiaize matrix
+	L = len(M)
+	D = np.zeros([L,L])
+
+	def find4(i,j):
+		answer = 0.0
+		k = i + 4
+		while(k <= (j-5)): 
+			answer = max(answer, D[i][k]+D[k+1][j])
+			# print(answer)
+			k += 1
+		return answer
+
+	#recursion
+	for i, e in reversed(list(enumerate(M))):
+		for j in range(i+3,L):
+			value1 = D[i+1][j]
+			value2 = D[i][j-1]
+			value3 = D[i+1][j-1]+M[i][j]
+			value4 = find4(i,j)
+			# print(value1,value2,value3,value4)
+			D[i][j] = max(value1,value2,value3,value4)
+	return D
+
+def BackTrace(g, M):
+    """
+    BackTrace - backtrace throug Nussinov matrix
+    g - matrix returned by Nussinov
+    seq - sequence string
+    return
+    pairs - pairs array; i, i+1 base paired
+    M - numpy pairs array M[i,j] == 1 if i and j base paired
+    """
+
+    def traceback(i, j):
+        """
+        traceback - recursive back trace through g matrix
+                    fills pairs matrix defined in outer scope
+        """
+        nonlocal pairs
+        
+        if i < j:
+            if g[i,j] == g[i+1, j]:
+                traceback(i+1, j)
+            elif g[i,j] == g[i, j-1]:
+                traceback(i, j-1)
+            elif g[i,j] == g[i+1, j-1] + M[i][j]:
+                if abs(i-j) > 2:
+                	pairs += [i,j]
+                traceback(i+1, j-1)
+            else:
+                for k in range(i+1, j):
+                    if g[i,j] == g[i, k] + g[k+1, j]:
+                        traceback(i, k)
+                        traceback(k+1, j)
+                        break
+    
+    L = g.shape[0]
+    pairs = []
+    traceback(0, L-1)
+
+    dot_bracket = np.zeros([L, L], dtype =int)
+    for i in range(0, len(pairs), 2):
+        dot_bracket[pairs[i], pairs[i+1]] = 1
+
+    return pairs, dot_bracket
+
+def StructureFromPairs(pairs, L):
+    struct = list('.' * L)
+    for i in range(0, len(pairs), 2):
+        struct[pairs[i]] = '('
+        struct[pairs[i+1]] = ')'
+        
+    return ''.join(struct)
 
 def main():
-
-	#parse in the PRP file
+	np.set_printoptions(threshold=np.nan)
+	#parse in the PRP file or alignment file
 	filename = "alignment.clw"
+	# filename = "PRP.fasta"
 	sequences,ids = parse(filename)
 	# print (sequences)
 
@@ -64,14 +203,40 @@ def main():
 	"""
 	count_matrix, consensus_seq = calculateConsensus(sequences)
 	# print (count_matrix)
-	# print(consensus_seq)
+	print("The consensus sequence is:")
+	print(consensus_seq)
+
+	"""
+	Convert count_matrix to a frequency_matrix
+	"""
+	frequency_matrix = convert(count_matrix)
+	# print(frequency_matrix)
 
 	"""
 	Calculate Mutual Information
 	Matrix Dimensions: column size x column size
 	"""
-	M = calculateMutualInfo(count_matrix)
-	print(M)
+	M = calculateMutualInfo(frequency_matrix,sequences)
+	# print(M)
+
+	"""
+	Calculate Maximum mutual information secondary structure using dynamic programming
+	"""
+	g = dynamic_program(M)
+	# print(g)
+
+	"""
+	Run back trace algorithm to find pairs and the secondary structure in dot-bracket format
+	"""
+	pairs, dot_bracket = BackTrace(g,M)
+	# print(pairs)
+	struct = StructureFromPairs(pairs, len(M))
+	print("\nThe dot-bracket format description of the consensuse sequence is:")
+	print(struct)
+
+	"""
+	Plot the 2d secondary structure w/ external source
+	"""
 
 	return 0
 
